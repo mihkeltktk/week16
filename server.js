0000516cf4284257ec8f06f6aeca63de518fe826
Mihkel TKTK <mihkel.osa@tktk.ee>
2026-05-17 00:47:36 +0300

    ejs lahenduse teostamine

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (request, reponse) => {
+
+
+app.get('/', (request, response) => {
     
-    reponse.sendFile(path.join(__dirname, 'index.html'));
+    const jsonFilePath = path.join(__dirname, 'data', 'cvData.json');
+
+    fs.readFile(jsonFilePath, (error, dataFromFile) => {
+        if(error) {
+            console.error('Failed to read from file', error);
+            return response.status(500).send('Failed to read from file.');
+        }
+
+        const jsonData = JSON.parse(dataFromFile);
+        response.render('index', {cv: jsonData} );
+
+    })
+
 });
 
 
